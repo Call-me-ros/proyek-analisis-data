b4467b9ef67a908129aab5bcda4a6c6618567382
Piros <firasmuhsaid@gmail.com>
2026-04-26 17:27:18 +0700

    Update data path for loading all_data.parquet

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,7 +20,7 @@ def create_daily_orders_df(df):
     return daily_orders_df
 
 # 1. Load Data
-all_df = pd.read_parquet("all_data.parquet")
+all_df = pd.read_parquet("dashboard/all_data.parquet")
 datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
 all_df.reset_index(inplace=True)
